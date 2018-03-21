48af081e88fc14ef6f648772f73b6893967ec0dc
Polina Sonina <sarun4ik7@yandex.ru>
2018-03-21 03:40:44 +0300

    start hw4

---
+++ homework_4/hw4.py
@@ -0,0 +1,9 @@
+def search_rabin_multi(text, patterns):
+    """
+    text - строка, в которой выполняется поиск
+    patterns = [pattern_1, pattern_2, ..., pattern_n] - массив паттернов, которые нужно найти в строке text
+    По аналогии с оригинальным алгоритмом, функция возвращает массив [indices_1, indices_2, ... indices_n]
+    При этом indices_i - массив индексов [pos_1, pos_2, ... pos_n], с которых начинаетй pattern_i в строке text.
+    Если pattern_i ни разу не встречается в строке text, ему соотвествует пустой массив, т.е. indices_i = []
+    """
+    pass
